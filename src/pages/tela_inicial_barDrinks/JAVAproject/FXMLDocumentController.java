810977232f32f221791e212b0095fdd46376c307
IsaSilveira <isamsilveira16@gmail.com>
2022-06-02 17:13:30 -0300

    Sprint5_View_TelaInicialBarDrinks #115

--- src/pages/tela_inicial_barDrinks/JAVAproject/FXMLDocumentController.java
+++ src/pages/tela_inicial_barDrinks/JAVAproject/FXMLDocumentController.java
@@ -6,17 +6,61 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 public class FXMLDocumentController implements Initializable {
     
     @FXML
-    private Label label;
-    
+    private Button buttonChat;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private Button buttonEnviarProposta;
+
+    @FXML
+    private Button buttonEnviarProposta2;
+
+    @FXML
+    private Button buttonEventos;
+
+    @FXML
+    private Button buttonHome;
+
+    @FXML
+    private Button buttonVejaEventos;
+
+    @FXML
+    private Label label;
+
+    @FXML
+    void onbuttonChatpressed(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onbuttonEnviarProposta2pressed(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onbuttonEnviarPropostapressed(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onbuttonEventospressed(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onbuttonHomepressed(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onbuttonVejaEventos2pressed(MouseEvent event) {
+
     }
     
     @Override
