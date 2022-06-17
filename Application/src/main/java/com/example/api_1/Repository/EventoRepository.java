2580961132329326dc06a2ba397be2931df28ab9
Pablo Magalhães <90854726+pabloaugusto17@users.noreply.github.com>
2022-06-17 08:41:26 -0300

    DELETE application

--- Application/src/main/java/com/example/api_1/Repository/EventoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.api_1.Repository;
-
-import com.example.api_1.Model.EventoModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventoRepository extends JpaRepository<EventoModel, Integer> {
-}
