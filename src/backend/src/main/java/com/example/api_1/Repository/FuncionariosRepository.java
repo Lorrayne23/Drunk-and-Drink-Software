cd10e6b554ae2b88f7c9ab744124c536203ac3a3
Pablo Magalhães <90854726+pabloaugusto17@users.noreply.github.com>
2022-06-24 08:47:38 -0300

    DELETE

--- src/backend/src/main/java/com/example/api_1/Repository/FuncionariosRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.api_1.Repository;
-
-import com.example.api_1.Model.FuncionariosModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FuncionariosRepository extends JpaRepository<FuncionariosModel, Integer> {
-}
